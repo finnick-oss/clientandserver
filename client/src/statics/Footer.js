237bc5e2ad9d2307cb679234db120d3fdf044ce9
Deepak <deepak988088@gmail.com>
2024-05-19 00:58:04 +0530

    footer update css

--- client/src/statics/Footer.js
+++ client/src/statics/Footer.js
@@ -5,13 +5,13 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <h1 className="contact-us-title">Contact Us</h1>
+        {/* <h1 className="contact-us-title">Contact Us</h1> */}
+        <p className='footer-copyright'>&copy; 2024 VisionVow. All rights reserved.</p>
         <div className="contact-info">
           <p><strong>Email:</strong> contact@example.com</p>
           <p><strong>Phone:</strong> +1 (123) 456-7890</p>
           <p><strong>Address:</strong> 123 Main Street, City, Country</p>
         </div>
-        <p>&copy; 2024 VisionVow. All rights reserved.</p>
       </div>
     </footer>
   );
